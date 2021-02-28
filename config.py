c6c0636dd5f000cafb42d67f6ad60a50f69b633a
tserekh <kvertea@gmial.com>
2021-02-28 23:01:18 +0300

    change to tensorflow 2.*

--- config.py
+++ config.py
@@ -1,13 +1,5 @@
 import os
-import mrcnn.config
-import mrcnn.utils
 from pathlib import Path
-class MaskRCNNConfig(mrcnn.config.Config):
-    NAME = "coco_pretrained_model_config"
-    IMAGES_PER_GPU = 1
-    GPU_COUNT = 1
-    NUM_CLASSES = 1 + 80  # COCO dataset has 80 classes + one background class
-    DETECTION_MIN_CONFIDENCE = 0.6
 
 ROOT_DIR = Path(".")
 MODEL_DIR = os.path.join(ROOT_DIR, "logs")
