2a5cba320e44de37898df2ce3e07ec0e6a552bd8
tserekh <kvertea@gmial.com>
2021-03-03 19:57:06 +0300

    delete show interface

--- ml.py
+++ ml.py
@@ -6,10 +6,7 @@ from functools import reduce
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from IPython.display import display
 from PIL import Image
-
-import config
 
 sys.path.append('/home/tserekh/exp/models/research/')
 from object_detection.utils import ops as utils_ops
@@ -35,33 +32,20 @@ def load_model(model_name):
 
 def run_inference_for_single_image(model, image):
     image = np.asarray(image)
-    # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
     input_tensor = tf.convert_to_tensor(image)
-    # The model expects a batch of images, so add an axis with `tf.newaxis`.
     input_tensor = input_tensor[tf.newaxis, ...]
-
-    # Run inference
     output_dict = model(input_tensor)
 
-    # All outputs are batches tensors.
-    # Convert to numpy arrays, and take index [0] to remove the batch dimension.
-    # We're only interested in the first num_detections.
     num_detections = int(output_dict.pop('num_detections'))
     output_dict = {key: value[0, :num_detections].numpy()
                    for key, value in output_dict.items()}
     output_dict['num_detections'] = num_detections
-
-    # detection_classes should be ints.
     output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
-
-    # Handle models with masks:
     if 'detection_masks' in output_dict:
-        # Reframe the the bbox mask to the image size.
         detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
             output_dict['detection_masks'], output_dict['detection_boxes'],
             image.shape[0], image.shape[1])
-        detection_masks_reframed = tf.cast(detection_masks_reframed > 0.5,
-                                           tf.uint8)
+        detection_masks_reframed = tf.cast(detection_masks_reframed > 0.5, tf.uint8)
         output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
 
     return output_dict
@@ -108,26 +92,3 @@ def recongnize(app, model, image_np, class_names_dict, category_index, now, exte
     df_gb.columns = ['sum', 'count']
     df_gb = df_gb.reset_index()
     return df_gb
-
-
-def show_inference(model, image_path):
-    # the array based representation of the image will be used later in order to prepare the
-    # result image with boxes and labels on it.
-
-    category_index = label_map_util.create_category_index_from_labelmap(config.PATH_TO_LABELS, use_display_name=True)
-
-    image_np = np.array(Image.open(image_path))
-    # Actual detection.
-    output_dict = run_inference_for_single_image(model, image_np)
-    # Visualization of the results of a detection.
-    vis_util.visualize_boxes_and_labels_on_image_array(
-        image_np,
-        output_dict['detection_boxes'],
-        output_dict['detection_classes'],
-        output_dict['detection_scores'],
-        category_index,
-        instance_masks=output_dict.get('detection_masks_reframed', None),
-        use_normalized_coordinates=True,
-        line_thickness=3)
-
-    display(Image.fromarray(image_np))
